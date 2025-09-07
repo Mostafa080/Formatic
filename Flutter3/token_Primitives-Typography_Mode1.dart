1b37b88600d9ff8223944db15e6f734c63cbca32
zeroheight <tokens-management@zeroheight.com>
2025-09-07 20:45:30 +0300

    Update Formatic 2026 from zeroheight

---
+++ Flutter3/token_Primitives-Typography_Mode1.dart
@@ -0,0 +1,19 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const contentFamilyFontFamilyBody = "IBM Plex Sans Arabic";
+    static const contentFamilyFontFamilyDisplay = "IBM Plex Sans Arabic";
+    static const contentWeightBold = "Bold";
+    static const contentWeightLight = "Light";
+    static const contentWeightMedium = "Medium";
+    static const contentWeightRegular = "Regular";
+    static const contentWeightSemibold = "Semibold";
+}
