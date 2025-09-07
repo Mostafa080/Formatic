8f49ce21741f38b344ee164803bcb340aec6c920
zeroheight <tokens-management@zeroheight.com>
2025-09-07 08:25:20 +0300

    Update Formatic 2026 from zeroheight

---
+++ Flutter3/token_Mapping_Dark.dart
@@ -0,0 +1,14 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorInvestmentAppIosButtonPrimaryBgColor = Color(0xFF121926);
+    static const colorInvestmentAppIosButtonSecondaryBgColor = Color(0xFF121926);
+}
