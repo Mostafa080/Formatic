f75a5559ac7da6b79f539b43188d04dbfdc02acf
zeroheight <tokens-management@zeroheight.com>
2025-09-09 20:39:46 +0300

    Update Formatic 2026 from zeroheight

---
+++ Flutter3/token_Semantic-Theme_LightTheme.dart
@@ -0,0 +1,106 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBackgroundActive = Color(0xFFF8FAFC);
+    static const colorBackgroundBrandPrimary = Color(0xFF5F09FF);
+    static const colorBackgroundBrandPrimaryAlt = Color(0xFFEBDDFB);
+    static const colorBackgroundBrandSecondary = Color(0xFFA76FFF);
+    static const colorBackgroundBrandSection = Color(0xFF4500C6);
+    static const colorBackgroundBrandSectionSubtle = Color(0xFF290073);
+    static const colorBackgroundBrandSolid = Color(0xFF5F09FF);
+    static const colorBackgroundBrandSolidHover = Color(0xFF4500C6);
+    static const colorBackgroundDisabled = Color(0xFFCDD5DF);
+    static const colorBackgroundDisabledSubtle = Color(0xFFF8FAFC);
+    static const colorBackgroundErrorPrimary = Color(0xFFFEF3F2);
+    static const colorBackgroundErrorSecondary = Color(0xFFFECDCA);
+    static const colorBackgroundErrorSolid = Color(0xFFF04438);
+    static const colorBackgroundErrorSolidHover = Color(0xFFB42318);
+    static const colorBackgroundOverlay = Color(0xFF121926);
+    static const colorBackgroundPrimary = Color(0xFFF8FAFC);
+    static const colorBackgroundPrimaryAlt = Color(0xFFFCFCFD);
+    static const colorBackgroundPrimaryHover = Color(0xFFF8FAFC);
+    static const colorBackgroundPrimarySolid = Color(0xFFFFFFFF);
+    static const colorBackgroundQuaternary = Color(0xFFE3E8EF);
+    static const colorBackgroundSecondary = Color(0xFFEEF2F6);
+    static const colorBackgroundSecondaryAlt = Color(0xFFEEF2F6);
+    static const colorBackgroundSecondaryHover = Color(0xFFEAECF5);
+    static const colorBackgroundSecondarySolid = Color(0xFF4B5565);
+    static const colorBackgroundSecondarySubtle = Color(0xFFF8FAFC);
+    static const colorBackgroundSuccessPrimary = Color(0xFFECFDF3);
+    static const colorBackgroundSuccessSecondary = Color(0xFFDCFAE6);
+    static const colorBackgroundSuccessSolid = Color(0xFF17BC6F);
+    static const colorBackgroundTertiary = Color(0xFFE3E8EF);
+    static const colorBackgroundWarningPrimary = Color(0xFFFFFAEB);
+    static const colorBackgroundWarningSecondary = Color(0xFFFEDF89);
+    static const colorBackgroundWarningSolid = Color(0xFFDC6803);
+    static const colorBorderBrand = Color(0xFF5F09FF);
+    static const colorBorderBrandAlt = Color(0xFF4500C6);
+    static const colorBorderDisabled = Color(0xFFCDD5DF);
+    static const colorBorderDisabledSubtle = Color(0xFFE3E8EF);
+    static const colorBorderError = Color(0xFFF04438);
+    static const colorBorderErrorSubtle = Color(0xFFFDA29B);
+    static const colorBorderPrimary = Color(0xFFE3E8EF);
+    static const colorBorderSecondary = Color(0xFFE3E8EF);
+    static const colorBorderSecondaryAlt = Color(0x80000000);
+    static const colorBorderTertiary = Color(0xFFEEF2F6);
+    static const colorForegroundBrandPrimary = Color(0xFF5F09FF);
+    static const colorForegroundBrandPrimaryAlt = Color(0xFF4500C6);
+    static const colorForegroundBrandSecondary = Color(0xFF5F09FF);
+    static const colorForegroundBrandSecondaryAlt = Color(0xFF4500C6);
+    static const colorForegroundBrandSecondaryHover = Color(0xFF4500C6);
+    static const colorForegroundDisabled = Color(0xFF9AA4B2);
+    static const colorForegroundDisabledSubtle = Color(0xFFCDD5DF);
+    static const colorForegroundErrorPrimary = Color(0xFFD92D20);
+    static const colorForegroundErrorSecondary = Color(0xFFF04438);
+    static const colorForegroundFgSuccessSecondary = Color(0xFF17BC6F);
+    static const colorForegroundPrimary = Color(0xFF121926);
+    static const colorForegroundQuaternary = Color(0xFF9AA4B2);
+    static const colorForegroundQuaternaryHover = Color(0xFF697586);
+    static const colorForegroundSecondary = Color(0xFF364152);
+    static const colorForegroundSecondaryHover = Color(0xFF202939);
+    static const colorForegroundSuccessPrimary = Color(0xFF079455);
+    static const colorForegroundTertiary = Color(0xFF4B5565);
+    static const colorForegroundTertiaryHover = Color(0xFF364152);
+    static const colorForegroundWarningPrimary = Color(0xFFDC6803);
+    static const colorForegroundWarningSecondary = Color(0xFFF79009);
+    static const colorForegroundWhite = Color(0xFFFFFFFF);
+    static const colorIconBrand = Color(0xFF5F09FF);
+    static const colorIconFeaturedError = Color(0xFFF04438);
+    static const colorIconFeaturedGray = Color(0xFF697586);
+    static const colorIconFeaturedSuccess = Color(0xFF17BC6F);
+    static const colorIconFeaturedWarning = Color(0xFFF79009);
+    static const colorIconOnBrand = Color(0xFFA76FFF);
+    static const colorIconPrimary = Color(0xFF697586);
+    static const colorTextBrandPrimary = Color(0xFF5F09FF);
+    static const colorTextBrandSecondary = Color(0xFF290073);
+    static const colorTextBrandSecondaryHover = Color(0xFF290073);
+    static const colorTextBrandTertiary = Color(0xFF4500C6);
+    static const colorTextBrandTertiaryAlt = Color(0xFF4500C6);
+    static const colorTextDisabled = Color(0xFF697586);
+    static const colorTextErrorPrimary = Color(0xFFD92D20);
+    static const colorTextErrorPrimaryHover = Color(0xFFB42318);
+    static const colorTextLink = Color(0xFF5F09FF);
+    static const colorTextPlaceholder = Color(0xFF697586);
+    static const colorTextPlaceholderSubtle = Color(0xFFCDD5DF);
+    static const colorTextPrimary = Color(0xFF202939);
+    static const colorTextPrimaryOnBrand = Color(0xFFFFFFFF);
+    static const colorTextQuaternary = Color(0xFF697586);
+    static const colorTextQuaternaryOnBrand = Color(0xFF9152FF);
+    static const colorTextSecondary = Color(0xFF697586);
+    static const colorTextSecondaryHover = Color(0xFF202939);
+    static const colorTextSecondaryOnBrand = Color(0xFFA76FFF);
+    static const colorTextSuccessPrimary = Color(0xFF079455);
+    static const colorTextTertiary = Color(0xFF4B5565);
+    static const colorTextTertiaryHover = Color(0xFF364152);
+    static const colorTextTertiaryOnBrand = Color(0xFFA76FFF);
+    static const colorTextWarningPrimary = Color(0xFFDC6803);
+    static const colorTextWhite = Color(0xFFFFFFFF);
+}
