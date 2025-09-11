92b50ebefea03e0bef2e3f0a7e0abdeb6552772f
zeroheight <tokens-management@zeroheight.com>
2025-09-11 10:26:59 +0300

    Update Formatic 2026 from zeroheight

---
+++ Flutter3/token_Semantic-Typography_IOS.dart
@@ -0,0 +1,67 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const contentBodyLgBoldWeight = "Bold";
+    static const contentBodyLgLightWeight = "Light";
+    static const contentBodyLgMedWeight = "Medium";
+    static const contentBodyLgRegWeight = "Regular";
+    static const contentBodyLgSemiBoldWeight = "Semibold";
+    static const contentBodyMdBoldWeight = "Bold";
+    static const contentBodyMdLightWeight = "Light";
+    static const contentBodyMdMedWeight = "Medium";
+    static const contentBodyMdRegWeight = "Regular";
+    static const contentBodyMdSemiBoldWeight = "Semibold";
+    static const contentBodySmBoldWeight = "Bold";
+    static const contentBodySmLightWeight = "Light";
+    static const contentBodySmMedWeight = "Medium";
+    static const contentBodySmRegWeight = "Regular";
+    static const contentBodySmSemiBoldWeight = "Semibold";
+    static const contentBodyXlBoldWeight = "Bold";
+    static const contentBodyXlLightWeight = "Light";
+    static const contentBodyXlMedWeight = "Medium";
+    static const contentBodyXlRegWeight = "Regular";
+    static const contentBodyXlSemiBoldWeight = "Semibold";
+    static const contentBodyXsBoldWeight = "Bold";
+    static const contentBodyXsLightWeight = "Light";
+    static const contentBodyXsMedWeight = "Medium";
+    static const contentBodyXsRegWeight = "Regular";
+    static const contentBodyXsSemiBoldWeight = "Semibold";
+    static const contentDisplay2xlBoldWeight = "Bold";
+    static const contentDisplay2xlLightWeight = "Light";
+    static const contentDisplay2xlMedWeight = "Medium";
+    static const contentDisplay2xlRegWeight = "Regular";
+    static const contentDisplay2xlSemiBoldWeight = "Semibold";
+    static const contentDisplayLgBoldWeight = "Bold";
+    static const contentDisplayLgLightWeight = "Light";
+    static const contentDisplayLgMedWeight = "Regular";
+    static const contentDisplayLgRegWeight = "Regular";
+    static const contentDisplayLgSemiBoldWeight = "Semibold";
+    static const contentDisplayMdBoldWeight = "Bold";
+    static const contentDisplayMdLightWeight = "Light";
+    static const contentDisplayMdMedWeight = "Medium";
+    static const contentDisplayMdRegWeight = "Regular";
+    static const contentDisplayMdSemiBoldWeight = "Semibold";
+    static const contentDisplaySmBoldWeight = "Bold";
+    static const contentDisplaySmLightWeight = "Light";
+    static const contentDisplaySmMedWeight = "Regular";
+    static const contentDisplaySmRegWeight = "Regular";
+    static const contentDisplaySmSemiBoldWeight = "Semibold";
+    static const contentDisplayXlBoldWeight = "Bold";
+    static const contentDisplayXlLightWeight = "Light";
+    static const contentDisplayXlMedWeight = "Medium";
+    static const contentDisplayXlRegWeight = "Regular";
+    static const contentDisplayXlSemiBoldWeight = "Semibold";
+    static const contentDisplayXsBoldWeight = "Bold";
+    static const contentDisplayXsLightWeight = "Light";
+    static const contentDisplayXsMedWeight = "Medium";
+    static const contentDisplayXsRegWeight = "Regular";
+    static const contentDisplayXsSemiBoldWeight = "Semibold";
+}
